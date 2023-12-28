828c0cfb955ab940e3ccac22847df01a2794dbcc
gerson <gerson@email.com>
2023-12-28 13:55:17 -0300

    feat: created the ms-catalog project structure

---
+++ ms-catalog/src/test/java/com/datadives/mscatalog/MsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datadives.mscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
