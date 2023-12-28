82d3224a734d59476984890344cb89028514eeec
gerson <gerson@email.com>
2023-12-28 14:31:35 -0300

    feat: added the CameCatalogEntity atributes

--- ms-catalog/src/main/java/com/datadives/mscatalog/entity/GameCatalogEntity.java
+++ ms-catalog/src/main/java/com/datadives/mscatalog/entity/GameCatalogEntity.java
@@ -1,4 +1,25 @@
 package com.datadives.mscatalog.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document("catalog")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameCatalogEntity {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private List<String> genres;
+    private String publisher;
+    private String developer;
+    private int score;
+    private int year;
 }
