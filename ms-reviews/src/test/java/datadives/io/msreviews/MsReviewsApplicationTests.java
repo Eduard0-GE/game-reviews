57bf8a26c0e844b8d5519f1949509f0498824d48
gerson <gerson@email.com>
2024-01-05 12:32:33 -0300

    feat: created the ms-reviews project structure

---
+++ ms-reviews/src/test/java/datadives/io/msreviews/MsReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package datadives.io.msreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
