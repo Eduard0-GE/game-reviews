0dad5f843159e511fcddb07510b6992179997810
gerson <gerson@email.com>
2024-01-05 14:02:16 -0300

    feat: added the AnaliseDto atributes and a new constructor

--- ms-reviews/src/main/java/datadives/io/msreviews/dto/AnaliseDto.java
+++ ms-reviews/src/main/java/datadives/io/msreviews/dto/AnaliseDto.java
@@ -1,4 +1,23 @@
 package datadives.io.msreviews.dto;
 
+import datadives.io.msreviews.model.Analise;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnaliseDto {
+    private String email;
+    private String jogo;
+    private String analise;
+    private short nota;
+
+    public AnaliseDto(Analise analise){
+        this.email = analise.getUsuarioId().getEmail();
+        this.jogo = analise.getJogo();
+        this.analise = analise.getAnalise();
+        this.nota = analise.getNota();
+    }
 }
