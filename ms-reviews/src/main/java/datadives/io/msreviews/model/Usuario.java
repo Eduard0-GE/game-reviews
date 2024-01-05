8891683f9acda08dff8d2862d135e818532a6c3d
gerson <gerson@email.com>
2024-01-05 12:54:44 -0300

    feat: adicionei as restriçoes do banco de dados dentro da classe Usuario

--- ms-reviews/src/main/java/datadives/io/msreviews/model/Usuario.java
+++ ms-reviews/src/main/java/datadives/io/msreviews/model/Usuario.java
@@ -13,8 +13,11 @@ import lombok.NoArgsConstructor;
 public class Usuario {
     @Id()
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usuario_id")
     private int usuarioId;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String senha;
     private String telefone;
 }
