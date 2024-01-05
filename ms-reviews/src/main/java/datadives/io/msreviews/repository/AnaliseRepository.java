14ab7a6bbc72aeaaec04f4de2036ba0c58fb5055
gerson <gerson@email.com>
2024-01-05 13:33:27 -0300

    feat: added the findAll method to the AnaliseRepository

--- ms-reviews/src/main/java/datadives/io/msreviews/repository/AnaliseRepository.java
+++ ms-reviews/src/main/java/datadives/io/msreviews/repository/AnaliseRepository.java
@@ -1,2 +1,13 @@
-package datadives.io.msreviews.repository;public class AnaliseRepository {
+package datadives.io.msreviews.repository;
+
+import datadives.io.msreviews.model.Analise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AnaliseRepository extends JpaRepository<Analise,Integer> {
+
+    List<Analise> findAll();
 }
