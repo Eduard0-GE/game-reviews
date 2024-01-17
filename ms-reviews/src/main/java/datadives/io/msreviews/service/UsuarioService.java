41b87393d0e5d7ea6344d83e1abe604ec085d9dd
gerson <gerson@email.com>
2024-01-17 18:59:32 -0300

    feat: implemented the create method inside the UsuarioService class

--- ms-reviews/src/main/java/datadives/io/msreviews/service/UsuarioService.java
+++ ms-reviews/src/main/java/datadives/io/msreviews/service/UsuarioService.java
@@ -3,6 +3,7 @@ package datadives.io.msreviews.service;
 import datadives.io.msreviews.model.Usuario;
 import datadives.io.msreviews.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -25,5 +26,13 @@ public class UsuarioService {
     return repository.findAll().stream()
             .map(Usuario::getEmail)
             .collect(Collectors.toList());
+    }
+
+    public void create(Usuario user){
+        try{
+            repository.save(user);
+        }catch (DataIntegrityViolationException e){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Usuario com mesmo email já existe");
+        }
     }
 }
