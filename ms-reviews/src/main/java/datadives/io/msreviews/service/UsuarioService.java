874f27a62baa425aec4ba0f2dd2f773960ea7e09
gerson <gerson@email.com>
2024-01-21 12:48:03 -0300

    refactor: updated the import organization of the UsuarioService class

--- ms-reviews/src/main/java/datadives/io/msreviews/service/UsuarioService.java
+++ ms-reviews/src/main/java/datadives/io/msreviews/service/UsuarioService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -16,10 +15,14 @@ import java.util.stream.Collectors;
 
 @Service
 public class UsuarioService {
-    @Autowired
     private UsuarioRepository repository;
-    @Autowired
     private UsuarioMapper mapper;
+
+    @Autowired
+    public UsuarioService(UsuarioRepository repository, UsuarioMapper mapper){
+        this.repository = repository;
+        this.mapper = mapper;
+    }
 
     public Usuario findByEmail(String email) {
         return repository.findByEmail(email).orElseThrow(
