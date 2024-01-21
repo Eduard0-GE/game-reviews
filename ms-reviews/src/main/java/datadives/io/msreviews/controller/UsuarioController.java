4425419f90d79f5aaef22bc6c0646040977f8643
gerson <gerson@email.com>
2024-01-21 12:54:38 -0300

    feat: implemented the CREATE, UPDATE and DELETE methods inside the UsuarioController

--- ms-reviews/src/main/java/datadives/io/msreviews/controller/UsuarioController.java
+++ ms-reviews/src/main/java/datadives/io/msreviews/controller/UsuarioController.java
@@ -2,12 +2,12 @@ package datadives.io.msreviews.controller;
 
 import datadives.io.msreviews.model.Usuario;
 import datadives.io.msreviews.repository.UsuarioRepository;
+import datadives.io.msreviews.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,19 +16,49 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/v1")
 public class UsuarioController {
     @Autowired
-    private UsuarioRepository repository;
+    private UsuarioService service;
 
     @GetMapping("/user/{email}")
     public ResponseEntity<Usuario> findByEmail(@PathVariable String email){
-        return ResponseEntity.ok().body(repository.findByEmail(email).get());
+        try {
+            return ResponseEntity.ok().body(service.findByEmail(email));
+        }catch (ResponseStatusException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/user")
     public ResponseEntity<List<String>> findAll(){
-        List<String> emails = repository.findAll().stream()
-                .map(Usuario::getEmail)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok().body(emails);
+        return ResponseEntity.ok().body(service.findAll());
     }
 
+    @PostMapping("/user")
+    public ResponseEntity<String> create(@RequestBody Usuario user){
+        try {
+            service.create(user);
+            return ResponseEntity.status(201).body("Usuario criado com sucesso");
+        }catch (ResponseStatusException e){
+            return new ResponseEntity<>("Usuário já existe", HttpStatus.CONFLICT);
+        }
+    }
+
+    @PutMapping("/user")
+    public ResponseEntity<String> update(@RequestBody Usuario user){
+        try {
+            service.update(user);
+            return ResponseEntity.status(200).body("Usuario atualizado com sucesso");
+        }catch (ResponseStatusException e){
+            return new ResponseEntity<>("Usuário não encontrado", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/user/email/{email}")
+    public ResponseEntity<String> deleteByEmail(@PathVariable String email){
+        try {
+            service.deleteByEmail(email);
+            return ResponseEntity.ok().body("Usuario deletado com sucesso");
+        }catch (ResponseStatusException e){
+            return new ResponseEntity<>("Falha ao deletar usuário", HttpStatus.NOT_FOUND);
+        }
+    }
 }
